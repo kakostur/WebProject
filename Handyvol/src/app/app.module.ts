d6c5c07e49d74423766a171f6fc11cc936e853a9
kakostur <tursynbaevakarakat@gmail.com>
2025-04-05 00:06:42 +0500

    Fix paths and add app.module.ts

--- Handyvol/src/app/app.module.ts
+++ Handyvol/src/app/app.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppComponent } from './app.component';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { EventsComponent } from './events/events.component';
+// import { AllComponent } from './all/all.component';
+// import { RouterModule } from '@angular/router';
 
-@NgModule({
-  declarations: [],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: []
-})
-export class AppModule { }
+// @NgModule({
+//   declarations: [
+//     AppComponent,  // Включаем в declarations
+//     HeaderComponent,
+//     FooterComponent,
+//     EventsComponent,
+//     AllComponent
+//   ],
+//   imports: [BrowserModule, RouterModule],  // Импортируем необходимые модули
+//   providers: [],
+//   bootstrap: [AppComponent]  // Включаем AppComponent для старта
+// })
+// export class AppModule { }
